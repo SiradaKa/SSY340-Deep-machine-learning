83fcc3acb5effe3fd53911087d09ec03fbe059bc
Oscar Dahlin <oscardahlin11@gmail.com>
2023-10-13 09:41:16 +0200

    general changes

--- attention_gate.py
+++ attention_gate.py
@@ -4,28 +4,69 @@ import torch.nn as nn
 import torch.nn.functional as F
 from unet_architecture import Up, DoubleConv, Down, OutConv
 
+class ConvBlock(nn.Module):
+    def __init__(self, ch_in, ch_out):
+        super().__init__()
+        self.conv = nn.Sequential(
+                                  nn.Conv2d(ch_in, ch_out,
+                                            kernel_size=3, stride=1,
+                                            padding=1, bias=True),
+                                  nn.BatchNorm2d(ch_out),
+                                  nn.ReLU(inplace=True),
+                                  nn.Conv2d(ch_out, ch_out,
+                                            kernel_size=3, stride=1,
+                                            padding=1, bias=True),
+                                  nn.BatchNorm2d(ch_out),
+                                  nn.ReLU(inplace=True),
+        )
+        
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class UpConvBlock(nn.Module):
+    def __init__(self, ch_in, ch_out):
+        super().__init__()
+        self.up = nn.Sequential(
+                                nn.Upsample(scale_factor=2),
+                                nn.Conv2d(ch_in, ch_out,
+                                         kernel_size=3,stride=1,
+                                         padding=1, bias=True),
+                                nn.BatchNorm2d(ch_out),
+                                nn.ReLU(inplace=True),
+        )
+        
+    def forward(self, x):
+        x = x = self.up(x)
+        return x
+
+
 
 class AttentionBlock(nn.Module):
     def __init__(self, f_g, f_l, f_int):
         super().__init__()
         
-        # Transformation for 'g' input
         self.w_g = nn.Sequential(
-            nn.Conv2d(f_g, f_int, kernel_size=1, stride=1, padding=0, bias=True),
-            nn.BatchNorm2d(f_int)
+                                nn.Conv2d(f_g, f_int,
+                                         kernel_size=1, stride=1,
+                                         padding=0, bias=True),
+                                nn.BatchNorm2d(f_int)
         )
         
-        # Transformation for 'x' input
         self.w_x = nn.Sequential(
-            nn.Conv2d(f_l, f_int, kernel_size=1, stride=1, padding=0, bias=True),
-            nn.BatchNorm2d(f_int)
+                                nn.Conv2d(f_l, f_int,
+                                         kernel_size=1, stride=1,
+                                         padding=0, bias=True),
+                                nn.BatchNorm2d(f_int)
         )
         
-        # Attention mask computation
         self.psi = nn.Sequential(
-            nn.Conv2d(f_int, 1, kernel_size=1, stride=1, padding=0,  bias=True),
-            nn.BatchNorm2d(1),
-            nn.Sigmoid()
+                                nn.Conv2d(f_int, 1,
+                                         kernel_size=1, stride=1,
+                                         padding=0,  bias=True),
+                                nn.BatchNorm2d(1),
+                                nn.Sigmoid(),
         )
         
         self.relu = nn.ReLU(inplace=True)
@@ -33,11 +74,10 @@ class AttentionBlock(nn.Module):
     def forward(self, g, x):
         g1 = self.w_g(g)
         x1 = self.w_x(x)
-        psi = self.relu(g1 + x1)
+        psi = self.relu(g1+x1)
         psi = self.psi(psi)
         
-        return psi * x
-
+        return psi*x
 
 class AttentionUNet(nn.Module):
     def __init__(self, n_classes=1, in_channel=3, out_channel=1):
@@ -45,27 +85,27 @@ class AttentionUNet(nn.Module):
         
         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
         
-        self.conv1 = DoubleConv(ch_in=in_channel, ch_out=64)
-        self.conv2 = DoubleConv(ch_in=64, ch_out=128)
-        self.conv3 = DoubleConv(ch_in=128, ch_out=256)
-        self.conv4 = DoubleConv(ch_in=256, ch_out=512)
-        self.conv5 = DoubleConv(ch_in=512, ch_out=1024)
+        self.conv1 = ConvBlock(ch_in=in_channel, ch_out=64)
+        self.conv2 = ConvBlock(ch_in=64, ch_out=128)
+        self.conv3 = ConvBlock(ch_in=128, ch_out=256)
+        self.conv4 = ConvBlock(ch_in=256, ch_out=512)
+        self.conv5 = ConvBlock(ch_in=512, ch_out=1024)
         
-        self.up5 = Up(ch_in=1024, ch_out=512)
+        self.up5 = UpConvBlock(ch_in=1024, ch_out=512)
         self.att5 = AttentionBlock(f_g=512, f_l=512, f_int=256)
-        self.upconv5 = DoubleConv(ch_in=1024, ch_out=512)
+        self.upconv5 = ConvBlock(ch_in=1024, ch_out=512)
         
-        self.up4 = Up(ch_in=512, ch_out=256)
+        self.up4 = UpConvBlock(ch_in=512, ch_out=256)
         self.att4 = AttentionBlock(f_g=256, f_l=256, f_int=128)
-        self.upconv4 = DoubleConv(ch_in=512, ch_out=256)
+        self.upconv4 = ConvBlock(ch_in=512, ch_out=256)
         
-        self.up3 = Up(ch_in=256, ch_out=128)
+        self.up3 = UpConvBlock(ch_in=256, ch_out=128)
         self.att3 = AttentionBlock(f_g=128, f_l=128, f_int=64)
-        self.upconv3 = DoubleConv(ch_in=256, ch_out=128)
+        self.upconv3 = ConvBlock(ch_in=256, ch_out=128)
         
-        self.up2 = Up(ch_in=128, ch_out=64)
+        self.up2 = UpConvBlock(ch_in=128, ch_out=64)
         self.att2 = AttentionBlock(f_g=64, f_l=64, f_int=32)
-        self.upconv2 = DoubleConv(ch_in=128, ch_out=64)
+        self.upconv2 = ConvBlock(ch_in=128, ch_out=64)
         
         self.conv_1x1 = nn.Conv2d(64, out_channel,
                                   kernel_size=1, stride=1, padding=0)
